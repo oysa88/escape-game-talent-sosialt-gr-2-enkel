ac256f232c776024bf02d4cae20c79bf4cbcf4fb
Øystein Salomonsen <oystein.salomonsen@tekniskmuseum.no>
2025-09-16 18:12:22 +0200

    Oppdater main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,12 +1,21 @@
-function oppsettRadio () {
-    radioNivå1 = 113
+function oppsettOppgaver () {
+    radioNivå1 = 149
     radioNivå2 = 126
     radioNivå3 = 138
-    radioNivå4 = 149
+    radioNivå4 = 112
     radioNivå5 = 159
+    kodeord1 = "VITENSENTER"
+    kodeord2 = "INDIA"
+    kodeord3 = "ELEMENTÆRPARTIKKEL"
+    kodeord4 = "NEWTON"
+    kodeord5 = "MASKINIST"
 }
 function nivåFullført () {
     basic.showIcon(IconNames.Yes, 0)
+music.beginMelody([
+        "C4:1", "E4:1", "G4:1", "C5:2",
+        "G4:1", "C5:4"
+    ], MelodyOptions.Once)
 for (let index = 0; index < 6; index++) {
         strip.showColor(neopixel.colors(NeoPixelColors.Green))
         basic.pause(200)
@@ -18,19 +27,7 @@ for (let index = 0; index < 6; index++) {
     nivå += 1
     basic.showNumber(nivå)
 }
-function spillFullført () {
-    basic.showIcon(IconNames.Heart, 0)
-strip.showRainbow(1, 360)
-    for (let index = 0; index < 240; index++) {
-        strip.rotate(1)
-        strip.show()
-        basic.pause(20)
-    }
-    strip.clear()
-    strip.show()
-    basic.clearScreen()
-}
-input.onButtonPressed(Button.AB, function () {
+input.onButtonPressed(Button.A, function () {
     nivå += -1
     basic.showNumber(nivå)
     if (nivå == 1) {
@@ -41,31 +38,64 @@ input.onButtonPressed(Button.AB, function () {
         radio.setGroup(radioNivå2)
     } else if (nivå == 4) {
         radio.setGroup(radioNivå4)
+    } else if (nivå == 5) {
+        radio.setGroup(radioNivå5)
     }
 })
+function spillFullført () {
+    basic.showIcon(IconNames.Heart, 0)
+music.beginMelody([
+        "C4:1", "E4:1", "G4:1", "C5:2",
+        "G4:1", "C5:4"
+    ], MelodyOptions.Once)
+strip.showRainbow(1, 360)
+    for (let index = 0; index < 240; index++) {
+        strip.rotate(1)
+        strip.show()
+        basic.pause(20)
+    }
+}
 radio.onReceivedString(function (receivedString) {
-    if (nivå == 1 && receivedString == "NEWTON") {
+    if (nivå == 1 && receivedString == kodeord1) {
         nivåFullført()
         radio.setGroup(radioNivå2)
-    } else if (nivå == 2 && receivedString == "FINLAND") {
+    } else if (nivå == 2 && receivedString == kodeord2) {
         nivåFullført()
         radio.setGroup(radioNivå3)
-    } else if (nivå == 3 && receivedString == "ELEMENTÆRPARTIKKEL") {
+    } else if (nivå == 3 && receivedString == kodeord3) {
         nivåFullført()
         radio.setGroup(radioNivå4)
-    } else if (nivå == 4 && receivedString == "VITENSENTER") {
+    } else if (nivå == 4 && receivedString == kodeord4) {
         nivåFullført()
         radio.setGroup(radioNivå5)
-    } else if (nivå == 5 && receivedString == "MASKINIST") {
+    } else if (nivå == 5 && receivedString == kodeord5) {
         spillFullført()
     } else {
         nivåFeilet()
     }
 })
+input.onButtonPressed(Button.B, function () {
+    nivå += 1
+    basic.showNumber(nivå)
+    if (nivå == 1) {
+        radio.setGroup(radioNivå1)
+    } else if (nivå == 2) {
+        radio.setGroup(radioNivå2)
+    } else if (nivå == 3) {
+        radio.setGroup(radioNivå2)
+    } else if (nivå == 4) {
+        radio.setGroup(radioNivå4)
+    } else if (nivå == 5) {
+        radio.setGroup(radioNivå5)
+    }
+})
 function nivåFeilet () {
-    for (let index = 0; index < 6; index++) {
-        basic.showIcon(IconNames.No, 0)
-strip.showColor(neopixel.colors(NeoPixelColors.Red))
+    music.beginMelody([
+        "A3:2", "F3:4"
+    ], MelodyOptions.Once)
+basic.showIcon(IconNames.No, 0)
+for (let index = 0; index < 6; index++) {
+        strip.showColor(neopixel.colors(NeoPixelColors.Red))
         basic.pause(200)
         strip.clear()
         strip.show()
@@ -74,6 +104,11 @@ strip.showColor(neopixel.colors(NeoPixelColors.Red))
     }
     basic.showNumber(nivå)
 }
+let kodeord5 = ""
+let kodeord4 = ""
+let kodeord3 = ""
+let kodeord2 = ""
+let kodeord1 = ""
 let radioNivå5 = 0
 let radioNivå4 = 0
 let radioNivå3 = 0
@@ -84,6 +119,6 @@ let strip: neopixel.Strip = null
 strip = neopixel.create(DigitalPin.P12, 24, NeoPixelMode.RGB)
 nivå = 1
 basic.showNumber(nivå)
-oppsettRadio()
+oppsettOppgaver()
 radio.setGroup(radioNivå1)
 strip.showColor(neopixel.colors(NeoPixelColors.Yellow))
